f1850b30a61483113131199af3f9d305c0cff04a
rjumle <119658466+rjumle@users.noreply.github.com>
2023-10-08 21:08:20 +0530

    Update streamlit_app.py
    update typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parent New Health Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text(‘Omega 3 & Blueberry Oatmeal’)
-streamlit.text(‘Kale, Spinach & Rocket Smoothie’)
-streamlit.text(‘Hard-Boiled Free-Range Egg’)
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
